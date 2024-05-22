aec74b0f81f32f6c686e37665fcc49b35d9b9f2f
SiddhantBobde <siddhantbobde1704@gmail.com>
2024-05-22 22:04:32 +0530

    sass setup

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='about-container'>
+        <h1>This is a about page</h1>
+        <p>My name is siddhant bobde and i like to write technical blogs.</p>
+    </div>
+  )
+}
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+	return (
+		<div className="home-container">
+			<h1 className='title'>This is home page</h1>
+			<p className='sub-title'>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam quaerat quia dolores ut non magni quibusdam sed molestias odio, reprehenderit
+				suscipit velit veritatis consequuntur, harum, officia ab similique inventore adipisci.
+			</p>
+		</div>
+	);
+};
+
+export default Home;
